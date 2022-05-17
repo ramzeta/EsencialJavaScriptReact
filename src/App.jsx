49b463d8a624a548b7481c8ab906e700f945cda6
ramiro13 <ramiroperezsanz13@gmail.com>
2022-05-17 17:39:14 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import  {React,useEffect,useState} from "react";
 
 export function App() {
     const nombre = "Ramiro";
@@ -79,6 +79,34 @@ export function App() {
     },[])
     console.log(elArrayreduce);
 
+    // Longhand
+    const obj1 = {
+        x: 1,
+        y: 2,
+        z: 3
+    }
+    
+    // Shorthand
+    const x = 8
+    const y = 10
+    const obj2 = { x, y }
+
+
+    //Optional chaining (?.)
+    const adventurer = {
+        name: 'Alice',
+        cat: {
+          name: 'Dinah'
+        }
+      };
+      
+      const dogName = adventurer.dog?.name;
+      console.log(dogName);
+      // expected output: undefined
+      
+      console.log(adventurer.someNonExistentMethod?.());
+      // expected output: undefined
+
     return (
         <>
             <h2>Cuanto Javascript necesito para React ?</h2>
@@ -89,23 +117,25 @@ export function App() {
                 <li>ShorThand en un objeto podemos ahorrar el clave valor </li>
                 <li>Arrow function, ahora lo tratamos como una variable ejemplo: <code style={styleCode}>constu funcionFlecha = () => return Hola</code> </li>
                 <li>Destructuring </li>
-                <li>Default Parameter antes se comprobaba con if ahora tenemos valores por defecto en funciones<code style={styleCode}> function (a=0,b=0) return a +b</code>  </li>
-                <li>La sintaxis de los parámetros rest nos permiten representar un número indefinido de argumentos como un array.
+                <li>Default Parameter antes se comprobaba con if ahora tenemos valores por defecto en funciones<code style={styleCode}> function (a=0,b=0) return a +b</code></li>
+                <li>La sintaxis <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax">Spread Operator</a>
                     <br />Antes usabamos metodos como concat y ahora
                     <code code style={styleCode}>
                         <br />
                         const array = [1,2,3]; <br />
                         const otroArray = [4,5,6]; <br />
-                        const nuevoArray = [...array,...otroArray] <br />
-
+                        const nuevoArray = [...array,...otroArray] <br /> <p>
+                            1,2,3,4,5,6
+                        </p>
                     </code>
                 </li>
-                <li>Imports ES6 <code> con desestructuracion para mejorar rendimiento y legibilidad de las importaciones</code></li>
+                <li>Imports ES6 <code> con desestructuracion para mejorar legibilidad de las importaciones</code></li> <br />
+                <p>No todos los navegadores comprenden ES6</p>
                 <li>Ternarios en React nos ayuda a hacer en JSX una validación o variantes <br />
                     <code>istrue? "si" : "no" //devolveria si la variable istrue es true = si sino no</code> <br />
                     Con esto podemos renderizar de forma customizada <code>{isTrue ? (<div>es correcto</div>) : (<div>No es correcto</div>)}</code> </li>
                 <li>Metodos arrays , find , some , every, includes, map , filter, reduce </li>
-                <li></li>
+                <li>Optional chaining (?.)</li>
             </ul>
         </>
     );
